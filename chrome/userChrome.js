e92d98c0f07372081d2b30faa334e3d94a8d7996
docrR <68521531+docrR@users.noreply.github.com>
2023-07-16 04:26:37 -0600

    Add files via upload

---
+++ chrome/userChrome.js
@@ -0,0 +1,9 @@
+// userChrome.js
+userChrome.import("/userChrome/restart_item_in_menu.uc.js", "UChrm");
+userChrome.import("/userChrome/favicon_in_urlbar.uc.js", "UChrm");
+userChrome.import("/userChrome/activity_throbber.uc.js", "UChrm");
+userChrome.import("/userChrome/space_and_separator_restorer.uc.js", "UChrm");
+userChrome.import("/userChrome/editbookmarkspopup_expanded.uc.js", "UChrm");
+userChrome.import("/userChrome/password_manager_button.uc.js", "UChrm");
+
+//userChrome.import("/userChrome/additional_top_toolbars_old.uc.js", "UChrm");//
